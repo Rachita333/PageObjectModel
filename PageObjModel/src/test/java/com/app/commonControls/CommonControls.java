0b326ed5bc69ac59cc824ac6908cca1ec5e46322
rrtundurwar <rrtundurwar@info-lap668.infocepts.com>
2020-10-02 09:06:31 +0530

    Added common control class

---
+++ PageObjModel/src/test/java/com/app/commonControls/CommonControls.java
@@ -0,0 +1,21 @@
+package com.app.commonControls;
+
+import org.openqa.selenium.WebDriver;
+
+public class CommonControls {
+	
+	
+	public void switchToFrame(WebDriver driver, String frameId)
+	{
+		driver.switchTo().frame(frameId);
+		
+	}
+	
+	
+	public void switchToDefaultContent(WebDriver driver, String frameId)
+	{
+		driver.switchTo().defaultContent();
+		
+	}
+
+}
